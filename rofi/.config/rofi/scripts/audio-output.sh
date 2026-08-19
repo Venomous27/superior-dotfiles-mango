a5d4d9e424fc421d071cfbe0dd8c1ad7e501a909
Venomous27 <venomousrudra27@gmail.com>
2026-08-19 18:06:15 +0530

    changed audio script (pulseaudio to pipewire)

--- rofi/.config/rofi/scripts/audio-output.sh
+++ rofi/.config/rofi/scripts/audio-output.sh
@@ -2,24 +2,31 @@
 
 declare -A MAP
 
-while IFS=$'\t' read -r name desc; do
-    MAP["$desc"]="$name"
+while IFS=$'\t' read -r id name; do
+    MAP["$name"]="$id"
 done < <(
-    pactl list sinks | awk '
-        /^Sink #/ {name=""; desc=""}
-        /Name:/ {sub(/^[[:space:]]*Name:[[:space:]]*/, ""); name=$0}
-        /Description:/ {
-            sub(/^[[:space:]]*Description:[[:space:]]*/, "")
-            desc=$0
-            print name "\t" desc
-        }'
+    wpctl status --name | awk '
+        /Sinks:/ { sinks=1; next }
+        /Sources:/ { sinks=0 }
+        sinks && match($0, /[0-9]+\./) {
+            id = substr($0, RSTART, RLENGTH-1)
+            print id
+        }
+    ' |
+    while read -r id; do
+        name=$(wpctl inspect "$id" 2>/dev/null |
+            awk -F'"' '/node.description/ {print $2; exit}')
+
+        [ -n "$name" ] && printf "%s\t%s\n" "$id" "$name"
+    done
 )
 
 choice=$(printf "%s\n" "${!MAP[@]}" | sort | rofi -dmenu \
-    -theme /home/venom/dotfiles/rofi/.config/rofi/themes/audio-output.rasi \
+    -theme "$HOME/superior-dotfiles-mango/rofi/.config/rofi/themes/audio-output.rasi" \
     -p "󰓃 Output")
+
 [ -z "$choice" ] && exit
 
-pactl set-default-sink "${MAP[$choice]}"
+wpctl set-default "${MAP[$choice]}"
 
 notify-send "Default output" "$choice"
